181d91d17bc59a94e19ee81aa85750561a2f00c3
digitopoi <hubo85@gmail.com>
2018-01-22 22:14:47 -0600

    Add interfaces

--- src/models/portfolio.interface.ts
+++ src/models/portfolio.interface.ts
@@ -1,7 +1,15 @@
+import { ChangeLog } from './changelog.interface';
+import { Social } from './social.interface';
+import { Updates } from './updates.interface';
+
 export interface Portfolio {
     firstName: string;
     middleName: string;
     lastName: string;
     subHeading?: string;
     socialMedia: Social[];
+    introduction: string;
+    changeLog: ChangeLog[];
+    updates: Updates[];
+    speakerBio: string;
 }
